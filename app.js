14928a7c02e360419551d52a645f85f594dd7e45
Fly J Gore <jennifer@theothergore.com>
2017-11-19 19:32:30 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var express = require('express')
 var app = express()
 
-// Build Your Route Here
-// app.get("/", function(req, resp){
-//   const viewData = {
-//     resp.render({
-//     name: "Jennifer",
-//     school: "Flatiron School"
-//     secret power: "Intuition"
-//   })}
-// 
-// })
+Build Your Route Here
+app.get("/", function(req, resp){
+  const viewData = {
+    resp.render({
+    name: "Jennifer",
+    school: "Flatiron School"
+    secret power: "Intuition"
+  })}
+
+})
 
 module.exports = app
